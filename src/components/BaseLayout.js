2dde1089b4cedde196f21480591febd05e314dfc
John <argote.john@gmail.com>
2019-02-20 12:04:03 -0600

    react-redux layout

---
+++ src/components/BaseLayout.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import Header from './Header';
+import Footer from './Footer'
+
+class BaseLayout extends React.Component {
+    constructor(props) {
+        super(props);
+        
+    }
+
+    render() {
+        return (
+            <div>
+                <Header />
+                <br/>
+                {this.props.children}
+                <br/>
+                <Footer />
+            </div>
+        );
+    }
+}
+
+
+export default BaseLayout
--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {createStore} from 'redux';
+import {Providor} from 'react-redux';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+
+import BaseLayout from './components/BaseLayout';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const store = createStore()
+
+
+ReactDOM.render(
+<Provider store={store}>
+    <BrowserRouter>
+        <BaseLayout>
+            <Switch>
+                <Route exact path="/" component={App} />
+            </Switch>
+        </BaseLayout>
+    </BrowserRouter>
+</Provider>    
+    , document.getElementById('root'));
+
